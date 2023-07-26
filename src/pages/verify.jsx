d9a5818699a6abb648f6eb2317651637405aa83d
faraygd <aldifahriziilmawan@gmail.com>
2023-07-26 22:02:24 +0700

    more testing

--- src/pages/verify.jsx
+++ src/pages/verify.jsx
@@ -1,67 +1,91 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-import { NFTAbi } from '../components/NftAbi.js';
+import { NFTAbi } from "../components/NftAbi.js";
 import { HeaderApps } from "../components/apps/HeaderApps.jsx";
-import QRCode from "qrcode.react";
+import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
 
-const verifyNFT = () => {
+const VerifyNFT = () => {
   const [isNFTHolder, setIsNFTHolder] = useState(false);
-  const [showBarcode, setShowBarcode] = useState(true);
-  const address = "0x854dB5c41419A42967DE4eC6A47C011b28B227eB";
-  console.log(address);
+  const [showScanButton, setShowScanButton] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const [scanSuccess, setScanSuccess] = useState(false);
+  const [scanning, setScanning] = useState(false);
+  const [inputAddress, setInputAddress] = useState(""); // State untuk menyimpan inputan alamat
   const contract = "0x1f1155BAd0CB7B9da4Ab7dD29091b614BEC7b6D1";
   const nftId = 0;
+  const address = useAddress();
+
+  const verifyNFTHolder = async () => {
+    try {
+      // Inisialisasi Web3 dengan node Ethereum di Testnet Mumbai (Matic)
+      const web3 = new Web3(
+        "https://rpc-mumbai.maticvigil.com/v1/a19a3cb1b68023800e1c769f750570dbd6d58fc4"
+      );
+      // Buat instance kontrak NFT berdasarkan alamat kontrak
+      const nftContract = new web3.eth.Contract(NFTAbi, contract);
+
+      // Panggil fungsi balanceOf di kontrak NFT untuk mendapatkan pemegang NFT dari NFT ID
+      const nftHolder = await nftContract.methods
+        .balanceOf(inputAddress, nftId) // Menggunakan inputAddress sebagai alamat NFT
+        .call();
+
+      // Jika nilai balik dari fungsi balanceOf bukan 0, berarti pemegang NFT valid
+      const isHolder = parseInt(nftHolder) > 0;
+      setIsNFTHolder(isHolder);
+      setShowResult(true); // Setelah verifikasi, tampilkan hasil
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleScanNow = () => {
+    setScanning(true);
+    // TODO: Implement QR code scanning logic here using a QR code scanner library
+    // For example, you can use "react-qr-reader" library to handle QR code scanning
+    // Once the scan is successful, set the scanSuccess state to true and setShowScanButton to false
+    // After that, proceed with the verification by calling verifyNFTHolder() function
+    // Note: The actual QR code scanning implementation is beyond the scope of this code snippet.
+  };
 
   useEffect(() => {
-    const verifyNFTHolder = async () => {
-      try {
-        // Inisialisasi Web3 dengan node Ethereum di Testnet Mumbai (Matic)
-        const web3 = new Web3(
-          "https://rpc-mumbai.maticvigil.com/v1/a19a3cb1b68023800e1c769f750570dbd6d58fc4"
-        );
-        // Buat instance kontrak NFT berdasarkan alamat kontrak
-        const nftContract = new web3.eth.Contract(NFTAbi, contract);
-
-        // Panggil fungsi balanceOf di kontrak NFT untuk mendapatkan pemegang NFT dari NFT ID
-        const nftHolder = await nftContract.methods
-          .balanceOf(address, nftId)
-          .call();
-
-        // Jika nilai balik dari fungsi balanceOf bukan 0, berarti pemegang NFT valid
-        const isHolder = parseInt(nftHolder) > 0;
-        setIsNFTHolder(isHolder);
-        setShowBarcode(false); // Setelah verifikasi, sembunyikan barcode
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-    if (!showBarcode) {
-      verifyNFTHolder();
+    if (address) {
+      setShowScanButton(true); // Show "Scan Now" button after the wallet is connected
     }
-  }, [showBarcode, address]);
+  }, [address]);
 
   return (
-    <div>
-        <HeaderApps/>
-      {showBarcode ? (
-        <div>
-          <QRCode value={contract} />
-          {/* Tampilkan barcode dengan nilai contract (alamat NFT) */}
-        </div>
-      ) : (
-        <div>
-          <ConnectWallet />
-          {isNFTHolder ? (
-            <h3>Pemegang NFT Valid!</h3>
-          ) : (
-            <h3>Pemegang NFT Tidak Valid!</h3>
+    <>
+      <HeaderApps />
+      <div className="bg-gray-500 min-h-screen">
+        <div className="items-center text-center p-12">
+          <h1 className="text-white font-bold text-2xl py-12">Verify Ticket</h1>
+          {!address ? (
+            <ConnectWallet />
+          ) : showScanButton ? (
+            <button
+              className="bg-blue-400 m-12 text-white w-24 h-12"
+              onClick={handleScanNow}
+              disabled={scanning}
+            >
+              {scanning ? "Scanning..." : "Scan Now"}
+            </button>
+          ) : null}
+          {showResult && (
+            <>
+              {!isNFTHolder && (
+                <h3 className="bg-green text-red-500 font-bold">
+                  Pemegang NFT Tidak Valid!
+                </h3>
+              )}
+              {isNFTHolder && (
+                <h3 className="text-green-500 font-bold">Pemegang NFT Valid!</h3>
+              )}
+            </>
           )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
-export default verifyNFT;
+export default VerifyNFT;

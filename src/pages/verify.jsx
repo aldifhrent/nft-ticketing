38e86e666444f2fff46f35c09412005cd8b549b8
faraygd <aldifahriziilmawan@gmail.com>
2023-07-27 00:22:54 +0700

    update RPC for web3

--- src/pages/verify.jsx
+++ src/pages/verify.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Web3 from "web3";
 import { NFTAbi } from "../components/NftAbi.js";
 import { HeaderApps } from "../components/apps/HeaderApps.jsx";
-import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
+import { useAddress } from "@thirdweb-dev/react";
+import Head from "next/head";
 
 const VerifyNFT = () => {
   const [isNFTHolder, setIsNFTHolder] = useState(false);
@@ -15,14 +16,14 @@ const VerifyNFT = () => {
     try {
       // Inisialisasi Web3 dengan node Ethereum di Testnet Mumbai (Matic)
       const web3 = new Web3(
-        "https://rpc-mumbai.maticvigil.com/v1/a19a3cb1b68023800e1c769f750570dbd6d58fc4"
+        "https://mumbai.rpc.thirdweb.com"
       );
       // Buat instance kontrak NFT berdasarkan alamat kontrak
       const nftContract = new web3.eth.Contract(NFTAbi, contract);
 
       // Panggil fungsi balanceOf di kontrak NFT untuk mendapatkan pemegang NFT dari NFT ID
       const nftHolder = await nftContract.methods
-        .balanceOf(address, nftId) // Menggunakan inputAddress sebagai alamat NFT
+        .balanceOf(address, nftId) // Menggunakan address sebagai alamat NFT dan nftId sebagai token id dari NFT
         .call();
 
       // Jika nilai balik dari fungsi balanceOf bukan 0, berarti pemegang NFT valid
@@ -35,35 +36,52 @@ const VerifyNFT = () => {
   };
   return (
     <>
-      <HeaderApps />
-      <div className="bg-gray-500 min-h-screen">
-        <div className="items-center text-center p-12">
-          <h1 className="text-white font-bold text-2xl py-12">Verify Ticket</h1>
-          <input type="text" placeholder="Masukkan address" value={address} />
-          <br />
-          <button
-            onClick={verifyNFTHolder}
-            className="bg-blue-500 text-white w-24 h-12 m-12"
-          >
-            Scan Now
-          </button>
-          <br />
-          {showResult && (
-            <>
-              {!isNFTHolder && (
-                <h3 className="bg-green text-red-500 font-bold">
-                  Pemegang NFT Tidak Valid!
-                </h3>
-              )}
-              {isNFTHolder && (
-                <h3 className="text-green-500 font-bold">
-                  Pemegang NFT Valid!
-                </h3>
-              )}
-            </>
-          )}
+      <Head>
+        <title>Verify Ticket</title>
+        <meta
+          name="description"
+          content="NFTicketing Experiment Using ThirdWeb"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <HeaderApps />
+        <div className="bg-gray-500 min-h-screen">
+          <div className="flex flex-col items-center">
+            <h1 className="text-white font-bold text-2xl py-12">
+              Verify Ticket
+            </h1>
+            <input
+              type="text"
+              placeholder="Address"
+              value={address}
+              className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-72 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <br />
+            <button
+              onClick={verifyNFTHolder}
+              className="bg-blue-500 text-white w-24 h-12 m-12 rounded-md"
+            >
+              Scan Now
+            </button>
+            {showResult && (
+              <>
+                {!isNFTHolder && (
+                  <h3 className="bg-red-500 font-bold text-white w-72 text-center flex">
+                    Non Verified Ticket Holders, buy ticket first
+                  </h3>
+                )}
+                {isNFTHolder && (
+                  <h3 className=" text-center p-auto bg-green-500 font-bold text-white w-72 flex">
+                    Verified Ticket Holders
+                  </h3>
+                )}
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 };

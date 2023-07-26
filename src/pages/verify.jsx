b557de49141d0d3ce784a106804ac0831a9b40d9
faraygd <aldifahriziilmawan@gmail.com>
2023-07-26 23:01:34 +0700

    update verify

--- src/pages/verify.jsx
+++ src/pages/verify.jsx
@@ -6,11 +6,7 @@ import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
 
 const VerifyNFT = () => {
   const [isNFTHolder, setIsNFTHolder] = useState(false);
-  const [showScanButton, setShowScanButton] = useState(false);
   const [showResult, setShowResult] = useState(false);
-  const [scanSuccess, setScanSuccess] = useState(false);
-  const [scanning, setScanning] = useState(false);
-  const [inputAddress, setInputAddress] = useState(""); // State untuk menyimpan inputan alamat
   const contract = "0x1f1155BAd0CB7B9da4Ab7dD29091b614BEC7b6D1";
   const nftId = 0;
   const address = useAddress();
@@ -26,7 +22,7 @@ const VerifyNFT = () => {
 
       // Panggil fungsi balanceOf di kontrak NFT untuk mendapatkan pemegang NFT dari NFT ID
       const nftHolder = await nftContract.methods
-        .balanceOf(inputAddress, nftId) // Menggunakan inputAddress sebagai alamat NFT
+        .balanceOf(address, nftId) // Menggunakan inputAddress sebagai alamat NFT
         .call();
 
       // Jika nilai balik dari fungsi balanceOf bukan 0, berarti pemegang NFT valid
@@ -37,39 +33,21 @@ const VerifyNFT = () => {
       console.error("Error:", error);
     }
   };
-
-  const handleScanNow = () => {
-    setScanning(true);
-    // TODO: Implement QR code scanning logic here using a QR code scanner library
-    // For example, you can use "react-qr-reader" library to handle QR code scanning
-    // Once the scan is successful, set the scanSuccess state to true and setShowScanButton to false
-    // After that, proceed with the verification by calling verifyNFTHolder() function
-    // Note: The actual QR code scanning implementation is beyond the scope of this code snippet.
-  };
-
-  useEffect(() => {
-    if (address) {
-      setShowScanButton(true); // Show "Scan Now" button after the wallet is connected
-    }
-  }, [address]);
-
   return (
     <>
       <HeaderApps />
       <div className="bg-gray-500 min-h-screen">
         <div className="items-center text-center p-12">
           <h1 className="text-white font-bold text-2xl py-12">Verify Ticket</h1>
-          {!address ? (
-            <ConnectWallet />
-          ) : showScanButton ? (
-            <button
-              className="bg-blue-400 m-12 text-white w-24 h-12"
-              onClick={handleScanNow}
-              disabled={scanning}
-            >
-              {scanning ? "Scanning..." : "Scan Now"}
-            </button>
-          ) : null}
+          <input type="text" placeholder="Masukkan address" value={address} />
+          <br />
+          <button
+            onClick={verifyNFTHolder}
+            className="bg-blue-500 text-white w-24 h-12 m-12"
+          >
+            Scan Now
+          </button>
+          <br />
           {showResult && (
             <>
               {!isNFTHolder && (
@@ -78,7 +56,9 @@ const VerifyNFT = () => {
                 </h3>
               )}
               {isNFTHolder && (
-                <h3 className="text-green-500 font-bold">Pemegang NFT Valid!</h3>
+                <h3 className="text-green-500 font-bold">
+                  Pemegang NFT Valid!
+                </h3>
               )}
             </>
           )}

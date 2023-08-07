7b2cb2b8eaf826c99bb272314bdbe1ec3bb749a1
faraygd <aldifahriziilmawan@gmail.com>
2023-08-07 13:54:33 +0700

    update wallet and version

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,8 @@ import {
   walletConnect,
   trustWallet,
   coinbaseWallet,
+  rainbowWallet,
+  zerionWallet,
 } from "@thirdweb-dev/react";
 import "../../styles/globals.css";
 import { chainId } from "../../const/mydetails";
@@ -16,25 +18,17 @@ export default function App({ Component, pageProps }) {
       supportedWallets={[
         metamaskWallet(),
         localWallet(),
+        rainbowWallet(),
+        zerionWallet(),
         paperWallet({
           clientId: "3012e8ba-000b-44d4-9b24-52050f2c6086",
-        }),
-        trustWallet(),
-        walletConnect(),
-        coinbaseWallet(),
+        })
       ]}
       clientId="e6389c507523eeafc24805e8a124b5c5"
       key="e6389c507523eeafc24805e8a124b5c5"
-      // sdkOptions={{
-      //   gasless: {
-      //     openzeppelin: {
-      //       relayerUrl:
-      //         "https://api.defender.openzeppelin.com/autotasks/e5ff2d3d-be21-4b02-9a12-9ef98d530657/runs/webhook/1b285739-9faf-4bf0-aa15-9b3a7bd99e55/P2D2fduQcNtHKUv9138D6N",
-      //     },
-      //   },
-      // }}
     >
       <Component {...pageProps} />
+      
     </ThirdwebProvider>
   );
 }

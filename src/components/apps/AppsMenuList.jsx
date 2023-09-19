d177cae9283a70c7e6ef6b1afc7e0580da275041
faraygd <aldifahriziilmawan@gmail.com>
2023-09-20 02:30:23 +0700

    update package

--- src/components/apps/AppsMenuList.jsx
+++ src/components/apps/AppsMenuList.jsx
@@ -1,29 +1,35 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import Link from "next/link";
+import { ConnectWallet } from "@thirdweb-dev/react";
 
 export const AppsMenuList = () => {
   return (
     <div>
       <ul className="hidden lg:flex gap-6 items-center">
-      <Link
-        className="text-gray-400 font-medium text-lg hover:text-gray-200"
-        href="/buyticket"
-      >
-        Buy Ticket
-      </Link>
-      <Link
-        className="text-gray-400 font-medium text-lg hover:text-gray-200"
-        href="/mynft"
-      >
-        My Ticket
-      </Link>
-      <Link
-        className="text-gray-400 font-medium text-lg hover:text-gray-200"
-        href="/verify"
-      >
-        Verify Ticket
-      </Link>
-      <ConnectWallet theme="white"/>
+        <Link
+          className="text-gray-400 font-medium text-lg hover:text-gray-200"
+          href="/buyticket"
+        >
+          Buy Ticket
+        </Link>
+        <Link
+          className="text-gray-400 font-medium text-lg hover:text-gray-200"
+          href="/mynft"
+        >
+          My Ticket
+        </Link>
+        <Link
+          className="text-gray-400 font-medium text-lg hover:text-gray-200"
+          href="/verify"
+        >
+          Verify Ticket
+        </Link>
+        <ConnectWallet
+          theme={"dark"}
+          btnTitle={"Connect Wallet"}
+          modalTitle={"NFTicketing"}
+          switchToActiveChain={true}
+          modalSize={"wide"}
+        />
       </ul>
     </div>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,18 +17,20 @@ export default function App({ Component, pageProps }) {
       activeChain={chainId}
       supportedWallets={[
         metamaskWallet(),
-        localWallet(),
-        rainbowWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        trustWallet(),
         zerionWallet(),
+        rainbowWallet(),
         paperWallet({
-          paperClientId: "3012e8ba-000b-44d4-9b24-52050f2c6086"
-        })
+          paperClientId: "3012e8ba-000b-44d4-9b24-52050f2c6086",
+        }),
+        localWallet(),
       ]}
       clientId="e6389c507523eeafc24805e8a124b5c5"
       key="e6389c507523eeafc24805e8a124b5c5"
     >
       <Component {...pageProps} />
-      
     </ThirdwebProvider>
   );
 }
